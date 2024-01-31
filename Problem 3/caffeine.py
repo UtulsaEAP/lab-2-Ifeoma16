36726529122da2c14ebea2b96a0e2e1c0a6198b6
Ifeoma16 <ifeomalockwood@gmail.com>
2024-01-31 01:10:16 -0600

    problem 3

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,7 +1,14 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm-3:15pm
+'''
 
 def caffeine():
     caffeine_mg = float(input())
     ''' Type your code here. '''
+    print("After 6 hours:", format((caffeine_mg/2), ".2f"), "mg")
+    print("After 12 hours:", format((caffeine_mg/4), ".2f"), "mg")
+    print("After 24 hours:", format((caffeine_mg/16), ".2f"), "mg")
     
 if __name__ == "__main__":
     caffeine()
