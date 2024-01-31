04e84e973f7d85e50f0e1b48938ac367bb6d5d1d
Ifeoma16 <ifeomalockwood@gmail.com>
2024-01-31 01:30:41 -0600

    problem 5

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -1,3 +1,7 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm-3:15pm
+'''
 
 def simple_stats():
     num1 = float(input())
@@ -5,5 +9,9 @@ def simple_stats():
     num3 = float(input())
     num4 = float(input())
     ''' Type your code here. '''
+    product = num1 * num2 * num3 * num4
+    average = (num1 + num2 + num3 + num4)/4
+    print(format(product, ".0f"), format(average, ".0f"))
+    print(format(product, ".3f"), format(average, ".3f"))
 if __name__ == "__main__":
     simple_stats()
